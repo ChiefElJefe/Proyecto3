87c846977b5be151d0c0fda1375e76a8faebb04e
Miguel <maarribas@bisnesmart.com>
2023-06-13 09:53:33 +0200

    Filtro entre registros

--- models/sale_line.py
+++ models/sale_line.py
@@ -6,7 +6,7 @@ class Sale_line(models.Model):
     _name = 'proyecto3.saleline'
     _description = 'proyecto3.saleline'
 
-    code_id = fields.Many2one('proyecto3.keycode', string="Code", domain="[('selled', '!=', True)]")
+    code_id = fields.Many2one('proyecto3.keycode', string="Code")
     key_code = fields.Char(string="Key code", related="code_id.key_code")
     creation_date = fields.Date(string="Creation Date", related="code_id.creation_date")
     sell_date = fields.Date(string="sell date", related="code_id.sell_date")
@@ -14,9 +14,9 @@ class Sale_line(models.Model):
     sello_id = fields.Many2one('proyecto3.codessells', string="Sell")
 
     @api.onchange('code_id')
-    def _key_filter(self):
+    def key_filter(self):
         used_code_ids = self.sello_id.keycode_ids.mapped('code_id').ids
-        domain = [('id', 'not in', used_code_ids)]
+        domain = [('id', 'not in', used_code_ids), ('selled', '!=', True)]
         return {'domain': {'code_id': domain}}
 
     # @api.constrains('sello_id')
